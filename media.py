47a24adde148835f6a6ceb8cdf26d57a3aefff3d
abhimanyusrivastava <abhimanyusrivastava@gmail.com>
2015-05-28 14:00:27 +0530

    Added Comments

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
 import webbrowser
+#importing python library webbrowser
 
 class Movie():
+	"""This Class provides a constructor method to store movie title, storyline, poster image and youtube trailer link. This Class also provides a method to open the youtube video in a browser"""
 	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
