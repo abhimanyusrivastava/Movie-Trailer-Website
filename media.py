7beccbbc934842eb4a66945282ab6b09586027aa
abhimanyusrivastava <abhimanyusrivastava@gmail.com>
2015-05-29 16:53:42 +0530

    Modified files as per PEP standards

--- media.py
+++ media.py
@@ -1,13 +1,17 @@
 import webbrowser
-#importing python library webbrowser
+# Importing python library webbrowser
+
 
 class Movie():
-	"""This Class provides a constructor method to store movie title, storyline, poster image and youtube trailer link. This Class also provides a method to open the youtube video in a browser"""
-	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url =  poster_image
-		self.trailer_youtube_url = trailer_youtube
+    """This Class provides a constructor method to store movie details \
+    and a method to open the youtube video in a browser"""
+    def __init__(
+      self,
+      movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
